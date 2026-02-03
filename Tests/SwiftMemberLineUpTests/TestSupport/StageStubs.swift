3064da3d4f5677b89cef6c7699f6920f251fa84f
Érico Perez Neto <ericodx@icloud.com>
2026-02-03 08:07:22 -0300

    test: add StageStubs

---
+++ Tests/SwiftMemberLineUpTests/TestSupport/StageStubs.swift
@@ -0,0 +1,31 @@
+@testable import SwiftMemberLineUp
+
+// MARK: - Integer Stages
+
+struct DoubleStage: Stage {
+    func process(_ input: Int) throws -> Int {
+        input * 2
+    }
+}
+
+struct AddTenStage: Stage {
+    func process(_ input: Int) throws -> Int {
+        input + 10
+    }
+}
+
+// MARK: - Conversion Stages
+
+struct ToStringStage: Stage {
+    func process(_ input: Int) throws -> String {
+        "\(input)"
+    }
+}
+
+// MARK: - Error Stages
+
+struct FailingStage: Stage {
+    func process(_ input: Int) throws -> Int {
+        throw TestError.intentional
+    }
+}
